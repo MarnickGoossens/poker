53cac51fd747e873769dadd21497c3651fed1498
Marnick Goossens <marnick.goossens.04@gmail.com>
2022-05-14 22:23:47 +0100

    probeersel

---
+++ src/poker/poker.jl
@@ -0,0 +1,48 @@
+using Random
+
+struct Card
+    suit::Int64
+    value::Int64
+    function Card(suit::Int64, value::Int64)
+        @assert(1 <= suit <= 4, "suit is not between 1 and 4")
+        @assert(1 <= value <= 13, "value is not between 1 and 13")
+        new(suit, value)
+    end
+end
+
+# Definitie van een kaart : Card(suit,value)
+
+const suit_name = ["♠", "♥", "♦", "♣"]
+const value_name = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+function Base.show(io::IO, card::Card)
+    print(io, value_name[card.value], suit_name[card.suit])
+end
+
+#making real deck
+struct Deck
+    cards::Array{Card,1}
+end
+
+function Deck()
+    deck = Deck(Card[])
+    for suit in 1:4
+        for value in 1:13
+            push!(deck.cards, Card(suit, value))
+        end
+    end
+    deck
+end
+
+function Random.shuffle!(deck::Deck)
+    shuffle!(deck.cards)
+    deck
+end
+shuffled_Deck = shuffle!(Deck())
+
+# prompt to input
+print("hoeveel spelers? \n\n")
+# Calling rdeadline() function
+aantel_spelers = readline()
+println("het aantal is:", aantel_spelers)
+print("\n\n")
